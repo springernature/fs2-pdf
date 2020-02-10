530c26d8a676e46546b0b7098a5a8ba11f777419
Torsten Schmits <torstenschmits@gmail.com>
2020-02-10 18:52:05 +0100

    improve pipeline sbt launcher

--- ops/pipeline-sbt.bash
+++ ops/pipeline-sbt.bash
@@ -5,21 +5,27 @@ set -euf -o pipefail
 here=$(dirname $0)
 cache_base='/var/halfpipe/shared-cache'
 cache="$cache_base/fs2-watermark"
-sbt_options=''
+jfrog_user="${artifactory_username:-$ARTIFACTORY_USERNAME}"
+jfrog_pass="${artifactory_password:-$ARTIFACTORY_PASSWORD}"
+sbt_options="-Dsbt.repository.config=$here/repo.properties"
+cached_dirs='.ivy2 .coursier .sbt'
+creds="Credentials(\"Artifactory Realm\", \"springernature.jfrog.io\", \"$jfrog_user\", \"$jfrog_pass\")"
 
 if [[ -d $cache_base ]]
 then
-  echo "Using cache directory $cache to build"
+  echo ">>> halfpipe sbt cache dir: $cache"
   mkdir -p $cache
-  rm -rf /root/.sbt
-  mkdir -p $cache/.sbt
-  ln -s $cache/.sbt /root/.sbt
-  sbt_options="-Dsbt.repository.config=$here/repo.properties"
+  for dir in $cached_dirs
+  do
+    rm -rf /root/$dir
+    mkdir -p $cache/$dir
+    ln -s $cache/$dir /root/$dir
+  done
 else
-  echo "Not using halfpipe cache"
+  echo ">>> Not using halfpipe cache for sbt"
 fi
 
 $here/sbt \
   $sbt_options \
-  'set every credentials := List(Credentials("Artifactory Realm", "springernature.jfrog.io", "'$artifactory_username'", "'$artifactory_password'"))' \
+  "set every credentials := List($creds)" \
   "$@"
