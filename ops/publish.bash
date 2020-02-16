9ada343983168e2144c748ab1282cbbb02cf4685
Torsten Schmits <torstenschmits@gmail.com>
2020-02-16 18:10:44 +0100

    echo version

--- ops/publish.bash
+++ ops/publish.bash
@@ -3,6 +3,7 @@
 set -eu -o pipefail
 
 version=$(sed -e 's/-SNAPSHOT//' -e 's/.*"\(.*\)"/\1/' version.sbt)
+echo ">>> updating readme version to $version"
 sed -i "s/\(.*\"fs2-pdf\" % \)".*"/\1 \"$version\"/" README.md
 
 sf-ci-publish-maven fs2-pdf master
