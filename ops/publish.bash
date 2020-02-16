5e3699fce9bcd7ca4cea4fc876b5c10bdf98f033
Torsten Schmits <torstenschmits@gmail.com>
2020-02-16 18:28:20 +0100

    commit readme version in publish

--- ops/publish.bash
+++ ops/publish.bash
@@ -5,5 +5,7 @@ set -eu -o pipefail
 version=$(sed -e 's/-SNAPSHOT//' -e 's/.*"\(.*\)"/\1/' version.sbt)
 echo ">>> updating readme version to $version"
 sed -i "s/\(.*\"fs2-pdf\" % \)".*"/\1 \"$version\"/" README.md
+git add README.md
+git commit -m "bump readme version"
 
 sf-ci-publish-maven fs2-pdf master
