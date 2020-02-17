2f7f3b02bf9814af39f42b1d064f050653f2e508
Torsten Schmits <torsten.schmits@springernature.com>
2020-02-17 14:08:19 +0100

    change trigger-publish to check the inputs of the job and poll until the commit is ready

--- ops/trigger-publish.bash
+++ ops/trigger-publish.bash
@@ -1,74 +1,81 @@
 #!/usr/bin/env bash
+# This script triggers the `publish` job of the specified concourse pipeline at the specified commit.
+# To prevent the wrong commit from being published, for example because a stage has failed without the dev noticing,
+# the script first calls the concourse api to obtain which commit would be processed, and waits for that commit to
+# match the specified commit (master is the default).
+# The user may force triggering the pipeline by pressing the `f` key while the script is polling.
 
 set -eu -o pipefail
 
 ref=${1:-master} pipeline=${2:-fs2-pdf}
+
+local_head=$(git rev-parse $ref)
 
 f()
 {
   fly -t sprcom "$@"
 }
 
-js()
+concourse_api()
+{
+  f curl "/api/v1/teams/sprcom/pipelines/$pipeline/$1" 2>/dev/null
+}
+
+# there is no way to print json strings without the quotes in jq
+json_string()
 {
   jq "$@" | sed 's/"//g'
 }
 
-ask()
+# the `inputs` of a job are resources like version and git.
+# the api endpoint returns those that would be processed if the job was started.
+# this function filters the git part of the array of resources `select(.type == "git")` and the accesses the `ref` field
+# in the object at the `version` field (.version.ref).
+query_git_ref='.[] | select(.type == "git") | .version.ref'
+publish_ref()
 {
-  local question=$1
-  read -n 1 -p "$question [y/n] " answer
-  echo
-  if [[ $answer != 'y' ]]
-  then
-    exit 1
-  fi
+  concourse_api "jobs/publish/inputs" | json_string "$query_git_ref"
 }
 
-ask_problem()
+# the main condition for a clean publish is that the tests have passed, which is fulfilled if the commit at the publish
+# job is the same as our target commit.
+ready()
 {
-  local problem=$1
-  ask "$problem. Publish anyway?"
+  [[ $(publish_ref) == $local_head ]]
 }
 
-job_attr()
+# loops in 1 second intervals (-t 1 option to `read`) to check whether the ref has arrived at the `publish` job
+poll()
 {
-  local name=$1 query=$2 j=${3:-jq}
-  f js -p $pipeline --json | $j ".[] | select(.name == \"$name\") | $query"
+  local answer
+  echo ">>> Waiting for the target ref to appear in the publish job's inputs."
+  echo ">>> Current commit is $(publish_ref)"
+  echo -n '>>> Press [f] to force trigger the pipeline with an older commit, [a] to abort: '
+  while true
+  do
+    if ready
+    then
+      return 0
+    fi
+    if read -t 1 -n 1 answer
+    then
+      echo ''
+      [[ $answer != 'f' ]]
+      return $?
+    fi
+  done
 }
 
-head=$(git rev-parse $ref)
-echo "Publishing from ref '$ref' at $head"
+trigger_publish()
+{
+  f tj -j $pipeline/publish -w
+}
 
-latest_in_pipeline=$(f rvs -r $pipeline/git --json | js '.[0].version.ref')
-if [[ $head != $latest_in_pipeline ]]
+echo ">>> Publishing with local ref '$ref' at $local_head"
+
+if ready || poll
 then
-  ask_problem "HEAD is not the latest commit in concourse ($latest_in_pipeline)"
+  trigger_publish
+else
+  echo '>>> Aborted by user.'
 fi
-
-publish_has_inputs=$(job_attr 'publish' '.has_new_inputs')
-if [[ $publish_has_inputs != 'true' ]]
-then
-  ask_problem 'Publish job does not have new inputs'
-fi
-
-next_test_build=$(job_attr 'test' '.next_build')
-if [[ $next_test_build != 'null' ]]
-then
-  ask_problem 'Test job is still running'
-fi
-
-test_build_status=$(job_attr 'test' '.finished_build.status' 'js')
-if [[ $test_build_status != 'succeeded' ]]
-then
-  ask_problem 'Test job failed'
-fi
-
-test_has_inputs=$(job_attr 'test' '.has_new_inputs')
-if [[ $test_has_inputs == 'true' ]]
-then
-  ask_problem 'Test job has new inputs'
-fi
-
-ask 'All conditions validated. Trigger publish job?'
-f tj -j $pipeline/publish -w
