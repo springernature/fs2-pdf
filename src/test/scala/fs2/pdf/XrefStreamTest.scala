403685eb8b867e33c174f162471fe6c9ce880711
Torsten Schmits <torsten.schmits@springernature.com>
2020-02-25 13:41:16 +0100

    regression test for xref streams

---
+++ src/test/scala/fs2/pdf/XrefStreamTest.scala
@@ -0,0 +1,19 @@
+package fs2
+package pdf
+
+import cats.effect.IO
+import org.specs2.mutable.Specification
+import scodec.bits.ByteVector
+
+class XrefStreamTest
+extends Specification
+{
+  def pipe(log: Log): Pipe[IO, Byte, ByteVector] =
+    PdfStream.elements(log)
+      .andThen(Element.encode)
+
+  "parse pdf" >>
+  ProcessJarPdf.ignoreError(ProcessJarPdf.processWith("xref-stream")(pipe))
+    .unsafeRunSync
+    .must_==(())
+}
