b8192791808cbd9b7d9385a82b317979cb360edd
Simone Puhle <simone.puhle@springernature.com>
2020-04-14 13:17:24 +0200

    added logging to find error in publish pipeline

--- ops/publish.bash
+++ ops/publish.bash
@@ -8,5 +8,6 @@ echo ">>> updating readme version to $version"
 sed -i "s/\(.*\"fs2-pdf\" % \)".*"/\1 \"$version\"/" README.md
 git add README.md
 git commit -m "bump readme version"
-
+echo ">>> publishing to maven"
 sf-ci-publish-maven fs2-pdf master
+echo ">>> published to maven"
