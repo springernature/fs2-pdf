7185c83e18d77b2f627fc3f42dac92ea9f44787e
Simone Puhle <simone.puhle@springernature.com>
2020-04-14 13:29:29 +0200

    commented out updating of readme to find error in publish pipeline

--- ops/publish.bash
+++ ops/publish.bash
@@ -2,12 +2,12 @@
 
 set -eu -o pipefail
 
-git switch master
-version=$(sed -e 's/-SNAPSHOT//' -e 's/.*"\(.*\)"/\1/' version.sbt)
-echo ">>> updating readme version to $version"
-sed -i "s/\(.*\"fs2-pdf\" % \)".*"/\1 \"$version\"/" README.md
-git add README.md
-git commit -m "bump readme version"
+#git switch master
+#version=$(sed -e 's/-SNAPSHOT//' -e 's/.*"\(.*\)"/\1/' version.sbt)
+#echo ">>> updating readme version to $version"
+#sed -i "s/\(.*\"fs2-pdf\" % \)".*"/\1 \"$version\"/" README.md
+#git add README.md
+#git commit -m "bump readme version"
 echo ">>> publishing to maven"
 sf-ci-publish-maven fs2-pdf master
 echo ">>> published to maven"
